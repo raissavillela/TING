1d29ca31d793baf67c92ea87f8930fad70a3143b
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-08-07 11:45:58 -0300

    Demais requisitos

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,34 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    new_queue = PriorityQueue()
+
+    high_priority = {"nome_do_arquivo": "file1.txt", "qtd_linhas": 3}
+    high_priority2 = {"nome_do_arquivo": "file2.txt", "qtd_linhas": 2}
+    regular_priority = {"nome_do_arquivo": "file3.txt", "qtd_linhas": 5}
+    regular_priority2 = {"nome_do_arquivo": "file4.txt", "qtd_linhas": 7}
+
+    new_queue.enqueue(regular_priority)
+    new_queue.enqueue(high_priority)
+    new_queue.enqueue(regular_priority2)
+    new_queue.enqueue(high_priority2)
+
+    assert len(new_queue) == 4
+
+    assert new_queue.dequeue() == high_priority
+    assert new_queue.dequeue() == high_priority2
+    assert new_queue.dequeue() == regular_priority
+    assert new_queue.dequeue() == regular_priority2
+
+    new_queue.enqueue(high_priority)
+    new_queue.enqueue(regular_priority)
+
+    assert new_queue.search(0) == high_priority
+    assert new_queue.search(1) == regular_priority
+
+    with pytest.raises(IndexError):
+        new_queue.search(-1)
+    with pytest.raises(IndexError):
+        new_queue.search(2)
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,36 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    word_lower = word.lower()
+
+    for index in range(len(instance)):
+        file_data = instance.search(index)
+        occurrences = []
+        for index, line in enumerate(file_data['linhas_do_arquivo']):
+            if word_lower in line.lower():
+                occurrences.append({"linha": index + 1})
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file_data["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    word_lower = word.lower()
+
+    for index in range(len(instance)):
+        file_data = instance.search(index)
+        occurrences = []
+        for index, line in enumerate(file_data['linhas_do_arquivo']):
+            if word_lower in line.lower():
+                occurrences.append({"linha": index + 1, "conteudo": line})
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file_data["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+    return result
